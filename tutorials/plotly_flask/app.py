fc7b568b8f520be163d7f298ca21c45be0d8d572
grei <vahfan@gmail.com>
2023-04-16 00:42:44 +1200

    this

--- tutorials/plotly_flask/app.py
+++ tutorials/plotly_flask/app.py
@@ -1,36 +1,74 @@
 from flask import Flask, render_template
 import plotly.graph_objs as go
 import plotly.io as pio
+import pandas as pd
+from flask_table import Table, Col
+import random
 
 app = Flask(__name__)
 
 
+class Data(Table):
+    x = Col("x")
+    y = Col("y")
+
+
 @app.route("/")
 def home():
-    # create the data
-    x = ["Apples", "Oranges", "Bananas"]
-    y = [10, 8, 12]
+    # # create the data
+    # x = ["Apples", "Oranges", "Bananas"]
+    # y = [10, 8, 12]
 
-    # create the bar chart
-    data = [go.Bar(x=x, y=y)]
+    # # create the bar chart
+    # data = [go.Bar(x=x, y=y)]
 
-    # create the layout
-    layout = go.Layout(
-        title="Fruit Sales",
-        xaxis={"title": "Fruit"},
-        yaxis={"title": "Sales"},
-    )
+    # # create the layout
+    # layout = go.Layout(
+    #     title="Fruit Sales",
+    #     xaxis={"title": "Fruit"},
+    #     yaxis={"title": "Sales"},
+    # )
 
-    # create the figure
-    fig = go.Figure(data=data, layout=layout)
+    # # create the figure
+    # fig = go.Figure(data=data, layout=layout)
 
-    fig.update_layout(plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)")
+    # create a sample dataframe
+    # data = {
+    #     "Name": ["Alice", "Bob", "Charlie"],
+    #     "Age": [25, 30, 35],
+    #     "City": ["New York", "San Francisco", "London"],
+    # }
+    # df = pd.DataFrame(data)
 
-    # export the figure to HTML
-    plot_div = pio.to_html(fig, full_html=False)
+    # # create a table using plotly
+    # fig = go.Figure(
+    #     data=[
+    #         go.Table(
+    #             header=dict(
+    #                 values=list(df.columns), fill_color="paleturquoise", align="left"
+    #             ),
+    #             cells=dict(
+    #                 values=[df.Name, df.Age, df.City],
+    #                 fill_color="lavender",
+    #                 align="left",
+    #             ),
+    #         )
+    #     ]
+    # )
 
-    # render the HTML template
-    return render_template("home.html", plot_div=plot_div)
+    data = []
+    for i in range(10):
+        data.append({"x": i, "y": random.randint(0, 10)})
+    table = Data(data)
+    return render_template("home.html", table=table)
+
+    # fig.update_layout(plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)")
+
+    # # export the figure to HTML
+    # plot_div = pio.to_html(fig, full_html=False)
+
+    # # render the HTML template
+    # return render_template("home.html", plot_div=plot_div)
 
 
 if __name__ == "__main__":
