69205e3d659ae7faea97c2272a74f90afe19f383
grei <vahfan@gmail.com>
2023-04-15 20:25:14 +1200

    plotly

---
+++ tutorials/plotly_flask/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import plotly.graph_objs as go
+import plotly.io as pio
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    # create the data
+    x = ["Apples", "Oranges", "Bananas"]
+    y = [10, 8, 12]
+
+    # create the bar chart
+    data = [go.Bar(x=x, y=y)]
+
+    # create the layout
+    layout = go.Layout(
+        title="Fruit Sales",
+        xaxis={"title": "Fruit"},
+        yaxis={"title": "Sales"},
+    )
+
+    # create the figure
+    fig = go.Figure(data=data, layout=layout)
+
+    # export the figure to HTML
+    plot_div = pio.to_html(fig, full_html=False)
+
+    # render the HTML template
+    return render_template("home.html", plot_div=plot_div)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ tutorials/plotly_flask/templates/home.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Plotly Bar Graph</title>
+</head>
+<body>
+    <div>
+        {{ plot_div|safe }}
+    </div>
+</body>
+</html>
