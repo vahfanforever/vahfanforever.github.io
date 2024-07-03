19ed293a5d91428d04791f078cf089e30d5c6f69
grei <vahfan@gmail.com>
2024-07-03 21:32:17 +1200

    updated image path to include project folder name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <img src="/frontpage.png" alt="Background" className="background-image" />
+      <img src="./website/frontpage.png" alt="Background" className="background-image" />
     </div>
   );
 }
