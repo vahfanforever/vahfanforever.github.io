b8e526f3e67dde298422f46fd5051715b96ce40a
grei <vahfan@gmail.com>
2024-07-03 19:54:28 +1200

    cleaned up website folder structure

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <img src="/frontpage.png" alt="Background" className="background-image" />
+    </div>
+  );
+}
+
+export default App;
