3446a3351a8d7d2cfcd6560ae098df0cdc64959f
grei <vahfan@gmail.com>
2024-07-03 19:56:54 +1200

    deleted all unused stuff

--- python_tutorials/javascript_flask/static/script.js
+++
@@ -1,5 +0,0 @@
-// const stats = document.getElementById('stats');
-// stats.innerHTML = "{{ ppg }} PPG | {{ppg}} FG% | {{ppg}} APG | {{ppg}} RPG";
-
-// const plotDiv = document.getElementById('plot');
-// plotDiv.innerHTML = "{{ plot_div|safe }}";
